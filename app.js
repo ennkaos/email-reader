989262f1a3559a1d313e745a9c49aa46786d102e
Ennkaos <xenon_1q2w@yahoo.com>
2021-07-15 16:47:15 +0200

    Second Push

--- app.js
+++ app.js
@@ -1,73 +1,87 @@
-const result=[]
 var Imap = require('imap'),
     inspect = require('util').inspect;
-    var imap = new Imap({
-        user: 'email@gmail.com',
-        password: 'password',
-        host: 'imap.gmail.com',
-        //imap.mail.yahoo.com - for yahoo
-        // imap.yourdomain.ext -for aruba
-        port: 993,
-        tls: true,
-        tlsOptions: {rejectUnauthorized: false}
-      });
-      
-
-function openInbox(cb) {
+let result = []
+async function openInbox(cb) {
     // openReadOnly = false
-    imap.openBox('Inbox', false, cb);
+    await imap.openBox('Inbox', false, cb);
 }
-
-imap.once('ready', function () {
-    openInbox(function (err, box) {
-        if (err) throw err;
-
-        // Search emails having "UNSEEN" property 
-        imap.search([ 'UNSEEN', ['SINCE', 'May 20, 2021'] ], function (err, results) {
-            if (err) throw err;
-            //try and catch block
-            try {
-                var f = imap.fetch(results, {bodies: 'TEXT'});
-                f.on('message', function (msg, seqno) {
-                    msg.on('body', function (stream, info) {
-                        var buffer = '';
-                        stream.on('data', function (chunk) {
-                            buffer += chunk.toString('utf8');
-                        });
-                        stream.once('end', function () {
-
-                            // Push ID and Date into Result
-                            msg.once('attributes', function (attrs) {
-
-                                let uid = attrs.uid;
-                                let date =attrs.date
-                                const object={uid,date}
-                                result.push(object)
-                               
-                            });
-
-                        });
-                    });
-                    console.log(result.length)
-                    return result.length
-                    
-                    
-                });
-               
-                f.once('end', function () {
-                    imap.end();
-                });
-                
-              
-            } catch (errorWhileFetching) {
-                console.log(errorWhileFetching.message);
-                imap.end();
-            }
-
-        });
-    });
+var imap = new Imap({
+    user: 'bucur.alexandru0106@gmail.com',
+    password: 'memako10',
+    host: 'imap.gmail.com',
+    //imap.mail.yahoo.com - for yahoo
+    // imap.yourdomain.ext -for aruba
+    port: 993,
+    tls: true,
+    tlsOptions: { rejectUnauthorized: false }
 });
 
+const test = async () => {
+    imap.connect();
 
+    const res = new Promise((resolve, reject) => {
+        imap.once('ready', () => {
+            openInbox((err, box) => {
+                if (err) throw err;
+        
+                // Search emails having "UNSEEN" property 
+                imap.search(['UNSEEN', ['SINCE', 'May 20, 2021']], (err, results) => {
+                    if (err) throw err;
+                    //try and catch block
+                    try {
+                        var f = imap.fetch(results, { bodies: 'TEXT' });
+                        f.on('message', async (msg, seqno) => {
+                            msg.on('body', function (stream, info) {
+                                var buffer = '';
+                                stream.on('data', function (chunk) {
+                                    buffer += chunk.toString('utf8');
+                                });
+                                stream.once('end', () => {
+        
+                                    // Push ID and Date into Result
+                                    msg.once('attributes', (attrs) => {
+        
+                                        let uid = attrs.uid;
+                                        let date = attrs.date
+                                        const object = { uid, date }
+                                        result.push(object)
+        
+                                    });
+        
+                                });
+                            });
+                            // console.log(result.length)
+                            return result.length
+        
+        
+                        });
+        
+                        f.once('end', () => {
+                            imap.end();
+                            return resolve(result);
+                        });
+        
+        
+                    } catch (errorWhileFetching) {
+                        // console.log(errorWhileFetching.message);
+                        imap.end();
+                        return reject();
+                    }
+        
+                });
+            });
+        });
+    });
+    
+    return res;
+}
 
-imap.connect();
+const withAsync = async () => {
+    console.log('before!');
+    await test()
+        .then(res => {
+            console.log('how many unread emails', res);
+        })
+        .catch(err => console.log(err));
+    console.log('afetr!');
+};
